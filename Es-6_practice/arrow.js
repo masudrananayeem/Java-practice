33fbecc3f9ea829058a3fd030335d504afe31efc
masudrananayeem <sadnayeem6393@gmail.com>
2026-06-17 01:44:45 +0600

    add

--- Es-6_practice/arrow.js
+++ Es-6_practice/arrow.js
@@ -3,9 +3,35 @@
     return num*2;
 }*/
 
-const doubleIt = function(num){
+/*const doubleIt = function(num){
     return num*2;
 }
+*/
+const doubleIt =(num) => num*2;
+
+const add=(x,y)=>x+y;
 
 const result = doubleIt(2);
+
+const result2= add(55,223);
+console.log(result2)
 console.log(result);
+
+
+const give4=()=>5;
+
+const math =(x,y)=>{
+
+    const sum =x+y;
+    const dif=x-y;
+    const result=sum*dif;
+    return result ;
+
+}
+
+
+const result3 =give4();
+console.log(result3);
+
+const result4 =math(33,22);
+console.log(result4);
