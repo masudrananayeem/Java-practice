88da1adb3571e35067f0d4038dcd936cd648ff3a
masudrananayeem <sadnayeem6393@gmail.com>
2026-06-17 01:31:51 +0600

    JUST add a file

---
+++ Es-6_practice/arrow.js
@@ -0,0 +1,11 @@
+/*function doubleIt(num){
+
+    return num*2;
+}*/
+
+const doubleIt = function(num){
+    return num*2;
+}
+
+const result = doubleIt(2);
+console.log(result);
--- Es-6_practice/template.js
+++ Es-6_practice/template.js
@@ -4,6 +4,14 @@ const fullName = firstName + " " + lastName;
 
 //const fullName2 = `${firstName} ${lastName} is a good boy`;
 const fullName2 = `${firstName} ${1552+156+545} is a good boy`;
-console.log(fullName2)
+console.log(fullName2);
 console.log(fullName);
+
+const multiline2=`Hlw hhlw
+tuki tuki tuki
+tuki tulu mulu.`
+
+console.log(multiline2);
+
+
 
