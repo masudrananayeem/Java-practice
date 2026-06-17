bab29ee5c78b2e49b43ae88ee1d4a58ad82e6670
masudrananayeem <sadnayeem6393@gmail.com>
2026-06-18 01:43:07 +0600

    practicing advance java

---
+++ Advanced-javascript/true-false.js
@@ -0,0 +1,23 @@
+const age = 0;
+
+if(age){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+const name = "solaiman";
+
+if(name.length > 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+/*falsey
+0, undefine, null, NaN*/
+
+/*Truthy
+"0", " ", */
